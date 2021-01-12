a92738f8aea9b413bd3ba0328cd24ba69d4bcc22
CoreNion <coreinion@gmail.com>
2021-01-12 15:25:10 +0900

    教材の動画のページで、ダイアログの背景色が黒くならない不具合を修正(N予備側の仕様変更？)

--- afterEvent.js
+++ afterEvent.js
@@ -1,25 +1,30 @@
 'use strict';
-const path = location.pathname;
-const body = $("body");
 
-//動画の教材での設定
-if (path.match(/movies/)) {
-  //理解度を設定ボタンを押したときに出るダイアログを黒くする
-  $("#root > div > footer > div > button").on("click", function () {
-    let interval = setInterval(function () {
-      if (body.find(".ReactModalPortal > div > div > div").length) {
-        body.find(".ReactModalPortal > div > div > div").eq(1).css("background-color", "#2c2c33");
+$(window).on("load", function () {
+  const path = location.pathname;
+  const body = $("body");
+
+  //動画の教材での設定
+  if (path.match(/movies/)) {
+    //理解度を設定ボタンが押された時に実行 
+    $("#root > div > footer > div > button").on("click", function () {
+      //理解度を設定するダイアログを黒くする
+      let interval = setInterval(function () {
+        if (body.find(".ReactModal__Content > div > div > div").length) {
+          body.find(".ReactModal__Content > div > div > div").css("background-color", "#2c2c33");
+          clearInterval(interval);
+        }
+      }, 3);
+      //3秒たっても出ない場合はIntervalを止める
+      setTimeout(function () {
         clearInterval(interval);
-      }
-    }, 3);
-    //3秒たっても出ない場合はIntervalを止める
-    setTimeout(function () {
-      clearInterval(interval);
-    }, 3000);
-  })
-  //動画の再生が終わったときに出る理解度のダイアログを黒くする
-  const video = document.querySelector('video');
-  video.onended = (event) => {
-    body.find(".ReactModalPortal > div > div > div").eq(1).css("background-color", "#2c2c33");
-  };
-}
+      }, 3000);
+    })
+    
+    //動画の再生が終わったときに出る理解度のダイアログを黒くする
+    const video = document.querySelector('video');
+    video.onended = (event) => {
+      body.find(".ReactModal__Content > div > div > div").css("background-color", "#2c2c33");
+    };
+  }
+});
