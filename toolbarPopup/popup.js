34f7a962f57adf4ff553f313fc5047c7a86b43b8
CoreNion <coreinion@gmail.com>
2021-04-10 17:27:27 +0900

    リビジョン番号のみのアップデートに対応

--- toolbarPopup/popup.js
+++ toolbarPopup/popup.js
@@ -1,22 +1,20 @@
 //DOMがロードされた後に実行するもの
-$(function(){
+$(function () {
   //ストレージから各種情報を取得
   chrome.storage.local.get(["latest", "lastCheck", "releaseURL"], function (data) {
     //アップデートがある場合
-    if(data.releaseURL){
+    if (data.releaseURL) {
       //アップデートの情報を表示
-      $("#updateStatus").html('<a href="'+ data.releaseURL +'" id="updateAvailable" target="_blank" rel="noopener noreferrer">アップデートがあります！ Version:' + data.latest + '</a>');
+      $("#updateStatus").html('<a href="' + data.releaseURL + '" id="updateAvailable" target="_blank" rel="noopener noreferrer">アップデートがあります！ Version:' + data.latest + '</a>');
     }
     //最終確認を更新(確認をしたことがある場合)
-    if(data.lastCheck){
-      $("#lastUpdateCheck").html(data.lastCheck);  
-    }    
+    if (data.lastCheck) {
+      $("#lastUpdateCheck").html(data.lastCheck);
+    }
   });
 
   //バージョンを表示
-  const manifest = chrome.runtime.getManifest();
-  const clientVer = Number(manifest.version);
-  $("#version").html(clientVer);
+  $("#version").html(chrome.runtime.getManifest().version);
 });
 
 /* アップデートを確認ボタンが押された時の動作 (Chrome拡張ではonclickが使えないのでjQuery経由で実行) */
@@ -30,12 +28,10 @@ $("#checkUpdateButton").on("click", function () {
   if (updateInt) {
     alert("アップデートがあります！ Version:" + updateInt);
     //Releaseページへのリンク
-    $("#updateStatus").html('<a href="'+ updateURL +'" id="updateAvailable" target="_blank" rel="noopener noreferrer">アップデートがあります！ Version:' + updateInt + '</a>');
+    $("#updateStatus").html('<a href="' + updateURL + '" id="updateAvailable" target="_blank" rel="noopener noreferrer">アップデートがあります！ Version:' + updateInt + '</a>');
     //最終更新を現在時刻へ更新
     $("#lastUpdateCheck").html(confirmationDate);
-    //ストレージに最終更新時刻を更新/追加
-    chrome.storage.local.set({'lastCheck': date.toLocaleString()});
-  } else if(updateInt == null){
+  } else if (updateInt == null) {
     alert("エラーが発生しました。\nインターネットに接続されているか確認してください。\nまたは、しばらくたってから、もう一度お試しください。");
     $("#updateStatus").html("アップデートはありません。");
   } else {
@@ -55,41 +51,53 @@ $("#checkUpdateButton").on("click", function () {
  *     アップデートの確認に失敗した場合:[null, null, null]
  */
 function hasUpdate() {
-  let latest = null;
+  let hasUpdate = false;
+  let latestVerStr = null;
+  let latestVerArray = null;
   let releaseURL = null;
   let nowTime = null;
-  //現在のバージョンを取得
-  const manifest = chrome.runtime.getManifest();
-  const clientVer = Number(manifest.version);
+
+  //現在のバージョンを取得し、配列化
+  const clientVerArray = chrome.runtime.getManifest().version.split(".");
   //最新バージョンが書かれているjsonを取得
   $.ajaxSetup({ async: false });
   //GithubのApiから最新バージョンを取得
   $.getJSON("https://api.github.com/repos/CoreNion/Ny0bi_Tool/releases/latest")
     .done(function (json) {
-      //tag_nameからタグの名前を取得し、数字のみにする
-      latest = json["tag_name"].replace(/[^0-9.]/g, '');
-      chrome.storage.local.set({'latest': latest});
-      //最新バージョンと比較し最新バージョンだったら0、そうじゃなかったらreleaseURLにURLを入れる
-      if (clientVer >= latest) {
-        latest = 0;
-        //ストレージのreleaseURLを削除
-        chrome.storage.local.remove('releaseURL', function (data) {});
-      } else {
-        releaseURL = json["html_url"];
-        //ストレージにreleaseURLを追加/更新
-        chrome.storage.local.set({'releaseURL': releaseURL});
+      //tag_nameからタグの名前を取得し、数字のみにし、配列化
+      latestVerStr = json["tag_name"].replace(/[^0-9.]/g, '');
+      latestVerArray = latestVerStr.split(".");
+
+      //最新バージョンと比較し、アップデートがあったらreleaseURLにURLを入れる
+      for (let i = 0; i <= latestVerArray.length; i++) {
+        console.log(latestVerArray[i] + " " + clientVerArray[i]);
+        if (!(latestVerArray[i] <= clientVerArray[i]) && !(latestVerArray[i] == undefined)) {
+          hasUpdate = true;
+          releaseURL = json["html_url"];
+          //ストレージにreleaseURLを追加/更新
+          chrome.storage.local.set({ 'releaseURL': releaseURL });
+        }
       }
+
+      //最新バージョンの値を保存
+      chrome.storage.local.set({ 'latest': latestVerArray });
+      //アップデートが無い時の処理
+      if (!hasUpdate) {
+        latestVerStr = 0;
+        //ストレージのreleaseURLを削除
+        chrome.storage.local.remove('releaseURL', function (data) { });
+      }
+
       //現在時刻を取得
       const date = new Date();
       nowTime = date.toLocaleString()
       //確認時刻をストレージに入れる
-      chrome.storage.local.set({'lastCheck': nowTime});
-      //現在時刻をnowTimeに入れる
+      chrome.storage.local.set({ 'lastCheck': nowTime });
     })
     .fail(function (err) {
       //エラー処理
       latest = null;
     });
   $.ajaxSetup({ async: true });
-  return [latest, releaseURL, nowTime];
+  return [latestVerStr, releaseURL, nowTime];
 }
