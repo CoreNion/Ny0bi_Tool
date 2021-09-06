8b98084a1dc485e689e69b060ae7ae7f835fe60f
CoreNion <coreinion@gmail.com>
2021-09-06 16:03:12 +0900

    [NewHome.js] 新ホームのトップページにダークモード適用完了
    あとはジャンルのページへの対応作業を行えば #3 は完了！

--- applyDarkNewHome.js
+++ applyDarkNewHome.js
@@ -28,6 +28,30 @@ function applyDarkNewHomePage() {
   //必修授業/課外授業表記とコース名の文字色
   classCSSPatcher($(courseListPath + " > div:nth-child(3) > div:nth-child(1) > h2"), "color: #e8e8e8;");
   classCSSPatcher($(courseListPath + " > div:nth-child(3) > div:nth-child(3) > a > h3"), "color: #e8e8e8;");
+
+  //右側の各h1タイトルにダークモード適用
+  classCSSPatcher($("#root > div > div:nth-child(2) > div > div:nth-child(2) > div > div:nth-child(1) > div:nth-child(1) > h1"),"color: #FFFFFF;");
+
+  //各コース/途中再開の部分にダークモード適用
+  const coursechapterLinkPath = "#root > div > div:nth-child(2) > div > div:nth-child(2) > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(1) > div > a > div";
+  //コース名
+  classCSSPatcher($(coursechapterLinkPath + " > div:nth-child(1) > div:nth-child(4) > div"),"background-color: #202124cc; color: #e8e8e8;");
+  //教材名や最終閲覧日時などが書かれている部分の背景色
+  classCSSPatcher($(coursechapterLinkPath + " > div:nth-child(2)"),"background-color: #202124;");
+  //教材名の文字色
+  classCSSPatcher($(coursechapterLinkPath + " > div:nth-child(2) > div:nth-child(1)"),"color: #FFFFFF;");
+  //コース名の文字色
+  classCSSPatcher($(coursechapterLinkPath + " > div:nth-child(2) > div:nth-child(2)"),"color: #e0e0e0;");
+
+  //フォーラムの各投稿の部分にダークモード適用
+  const forumLinkDivPath =  "#root > div > div:nth-child(2) > div > div:nth-child(2) > div > div:nth-child(9) > div";
+  //背景色
+  classCSSPatcher($(forumLinkDivPath),"background-color: #202124;");
+  //投稿の文字色
+  classCSSPatcher($(forumLinkDivPath + " > a"),"color: #e8e8e8");
+  classCSSPatcher($(forumLinkDivPath + " > a"),"color: #e8e8e8", "hover");
+  //投稿日時の文字色
+  classCSSPatcher($(forumLinkDivPath + " > a > div:nth-child(2) > div"),"color: #CFCFCF;");
 }
 
 function applyDarkMyCourcePage() {
